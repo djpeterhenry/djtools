62995b92e6d0a8e7aa6ab9a9658f1ea685650928
peter <phenry@gmail.com>
2016-06-10 08:23:32 -0700

    turn off dangerous rsync_x

--- rsync_x.sh
+++ rsync_x.sh
@@ -2,4 +2,6 @@
 # 3601 is a daylight savings time hack
 # not needed for new drive!
 # except when DST starts again!
-rsync -av --modify-window=3601 --delete "/Users/peter/Music/Ableton/User Library" $1
+
+# --delete was brutal when you targeted home folder, say
+#rsync -av --modify-window=3601 --delete "/Users/peter/Music/Ableton/User Library" $1
