cee807e899d59bb4afc58c05c2b733dd02b2f20a
peter <phenry@gmail.com>
2016-06-14 22:25:12 -0700

    removed offending rsync_x

--- rsync_x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# 3601 is a daylight savings time hack
-# not needed for new drive!
-# except when DST starts again!
-
-# --delete was brutal when you targeted home folder, say
-#rsync -av --modify-window=3601 --delete "${PROJECT_PATH}" $1
