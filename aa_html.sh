12a1bfc5b36fd3f0e978eab4bd0ef5945c00b670
peter <phenry@gmail.com>
2016-06-15 08:35:57 -0700

    meaningless html

--- aa_html.sh
+++ aa_html.sh
@@ -5,4 +5,4 @@ ableton_aid.py aadb.txt -html_list > list.html
 ableton_aid.py aadb.txt -html_list_num > list_num.html
 ableton_aid.py aadb.txt -html_list_sample > list_sample.html
 ableton_aid.py aadb.txt -html_list_alc > list_alc.html
-cp *.html ~/dropbox/DJ\ Peter\ Henry/
+cp *.html /Users/peter/Dropbox/DJ\ Peter\ Henry/
