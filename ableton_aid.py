5b7b5b05018cd9efc471d98a5450f07a51bd6631
peter <phenry@gmail.com>
2019-07-03 09:53:26 -0700

    wip

--- ableton_aid.py
+++ ableton_aid.py
@@ -42,8 +42,8 @@ def get_ts_for(year, month, day):
 
 OLD_ALC_TS_CUTOFF = get_ts_for(2016, 6, 12)
 
-USE_REKORDBOX_SAMPLE = True
-VERSION = 27
+USE_REKORDBOX_SAMPLE = False
+VERSION = 28
 
 REKORDBOX_SAMPLE_PATH = u'/Volumes/MacHelper/rekordbox_samples'
 #REKORDBOX_SAMPLE_PATH = u'/Volumes/music/rekordbox_samples'
