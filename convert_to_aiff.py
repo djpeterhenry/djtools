07dc586ab8a947dd87d26ac9e096aab2cb2a4ffb
peter <phenry@gmail.com>
2021-06-02 01:05:42 -0700

    wip

--- convert_to_aiff.py
+++ convert_to_aiff.py
@@ -4,17 +4,24 @@ import os
 import argparse
 import subprocess
 
+def convert_filename(input_filename):
+    base, extension = os.path.splitext(input_filename)
+    output_filename = base + " (from {})".format(extension) + ".aiff"
+    cmd = ['ffmpeg',
+        '-i', input_filename,
+        "-map_metadata", "0", # doesn't preserve tags as it should
+        output_filename
+    ]
+    subprocess.check_call(cmd)
+
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_filename')
     args = parser.parse_args()
 
-    input_filename = args.input_filename
-    base, extension = os.path.splitext(input_filename)
-    output_filename = base + " (from {})".format(extension) + ".aiff"
-
-    cmd = ['ffmpeg', '-i', input_filename, output_filename]
-    subprocess.check_call(cmd)
+    convert_filename(args.input_filename)
     
 if __name__ == '__main__':
     main()
