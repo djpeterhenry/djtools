67b12b5aff9b1cc84e06cfd07cff7c31a62fe29c
peter <phenry@gmail.com>
2021-06-05 14:33:52 -0700

    convert folder

--- convert_to_aiff.py
+++ convert_to_aiff.py
@@ -14,14 +14,23 @@ def convert_filename(input_filename):
     ]
     subprocess.check_call(cmd)
 
-
+def convert_folder(input_folder):
+    for filename in os.listdir(input_folder):
+        if os.path.splitext(filename)[1].lower() != ".mp3":
+            continue
+        convert_filename(filename)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_filename')
+    parser.add_argument('--input_filename')
+    parser.add_argument('--input_folder')
     args = parser.parse_args()
 
-    convert_filename(args.input_filename)
+    if args.input_filename:
+        convert_filename(args.input_filename)
+
+    if args.input_folder:
+        convert_folder(args.input_folder)
     
 if __name__ == '__main__':
     main()
