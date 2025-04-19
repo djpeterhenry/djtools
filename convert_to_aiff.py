e76146df2325f39d1f5857b1a000e2b5399a4cf8
Peter Henry <phenry@gmail.com>
2025-04-19 16:50:48 -0700

    Can convert to 16 bit.  Thanks copilot

--- convert_to_aiff.py
+++ convert_to_aiff.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.11
 
 import os
-import argparse
 import subprocess
+import argh
 
 
-def convert_filename(input_filename):
+def convert_filename(input_filename, pcm_16bit=False):
     base, extension = os.path.splitext(input_filename)
     output_filename = base + " (from {})".format(extension) + ".aiff"
     cmd = [
@@ -14,30 +14,30 @@ def convert_filename(input_filename):
         input_filename,
         "-map_metadata",
         "0",  # doesn't preserve tags as it should
-        output_filename,
     ]
+    if pcm_16bit:
+        cmd.extend(["-c:a", "pcm_s16le"])  # Specify 16-bit PCM encoding
+    cmd.append(output_filename)
     subprocess.check_call(cmd)
 
 
-def convert_folder(input_folder):
+def convert_folder(input_folder, pcm_16bit=False):
     for filename in os.listdir(input_folder):
         if os.path.splitext(filename)[1].lower() != ".mp3":
             continue
-        convert_filename(os.path.join(input_folder, filename))
+        convert_filename(os.path.join(input_folder, filename), pcm_16bit=pcm_16bit)
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input_filename")
-    parser.add_argument("--input_folder")
-    args = parser.parse_args()
+@argh.arg("--input-filename", help="Path to the input file to convert")
+@argh.arg("--input-folder", help="Path to the folder containing files to convert")
+@argh.arg("--pcm-16bit", help="Convert output to 16-bit PCM", default=False)
+def main(input_filename=None, input_folder=None, pcm_16bit=False):
+    if input_filename:
+        convert_filename(input_filename, pcm_16bit=pcm_16bit)
 
-    if args.input_filename:
-        convert_filename(args.input_filename)
-
-    if args.input_folder:
-        convert_folder(args.input_folder)
+    if input_folder:
+        convert_folder(input_folder, pcm_16bit=pcm_16bit)
 
 
 if __name__ == "__main__":
-    main()
+    argh.dispatch_command(main)
